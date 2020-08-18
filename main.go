724549cf19685bbcdc2fe30d2b7788117ef5f23d
bug <2532917800@qq.com>
2020-08-18 13:34:54 +0800

    init

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	//设置gin的运行模式
 	switch viper.GetString("ENV") {
 	case PRODUCTION_ENV:
-		gin.SetMode(gin.DebugMode)
+		gin.SetMode(gin.ReleaseMode)
 	case DEVELOPMENT_ENV:
 		gin.SetMode(gin.DebugMode)
 		//额外放置一个可以在控制台打印access_log的中间件
